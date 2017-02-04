274067b339aa69a710b1ba73f658237057ab88d8
Erik Nelson <erik@nsk.io>
2017-02-04 16:48:23 -0500

    Shuffle around main options
    * Removed required, positional project. Didn't really make sense when
    running in server mode

--- src/main.py
+++ src/main.py
@@ -6,13 +6,14 @@ from govr.cmd import *
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("project", help="Project under test")
-
 	parser.add_argument("-r", "--report",
 		help="Prints coverage report", action="store_true")
 
 	parser.add_argument("-t", "--travis",
 		help="Run as client under travis ci", action="store_true")
+
+	parser.add_argument("-p", "--project",
+			help="Project under test, required by -r and -t")
 
 	parser.add_argument("-s", "--server",
 		help="Run as server", action="store_true")
@@ -24,15 +25,21 @@ if __name__ == "__main__":
 		help="Run server in debug mode", action="store_true")
 	parser.add_argument("--server-state-dir",
 		help="File where runtime state is stored", default="/var/lib/govr")
+	parser.add_argument("--server-git-repo",
+		help="Clonable git path to repo under test", default="")
 
 	args = parser.parse_args()
 
 	print "Running against project: %s" % args.project
 
 	if args.travis:
+		if args.project == "":
+			raise Exception("ERROR: Running in travis mode requires a --project path to test")
 		Travis(args).run()
 	elif args.server:
 		"Running server"
 		Server(args).run()
 	else:
+		if args.project == "":
+			raise Exception("ERROR: Running in report mode requires a --project path to test")
 		print Report(args).run()
